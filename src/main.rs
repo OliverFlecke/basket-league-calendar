434f34393305e6dd76bcecbf4c5d209baa61a340
Oliver Fleckenstein <oliverfl@live.dk>
2023-09-16 23:57:45 +0100

    feat: Extracting all games with location and time

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,108 @@
-use chrono::{Duration, Utc};
+use chrono::{DateTime, FixedOffset, NaiveDateTime};
+use chrono_tz::Europe::Copenhagen;
 use icalendar::{Calendar, Class, Event, Property, *};
-use std::{fs::File, io::Write};
+use std::{fs::File, io::Write, time::Duration};
+use thirtyfour::prelude::*;
 
-fn main() -> anyhow::Result<()> {
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    load_page().await?;
+    Ok(())
+}
+
+const URL: &str = "https://basketligaen.dk/kampprogram";
+
+async fn load_page() -> anyhow::Result<()> {
+    let mut caps = DesiredCapabilities::firefox();
+    caps.set_headless()?;
+    // caps.set_no_sandbox()?;
+    let driver = WebDriver::new("http://localhost:4444", caps).await?;
+
+    driver.goto(URL).await?;
+
+    let elements = driver
+        .query(By::ClassName("match-wrap"))
+        .wait(Duration::from_secs(5), Duration::from_millis(100))
+        .all()
+        .await?;
+
+    let mut matches = Vec::new();
+    for element in elements {
+        if let Some(game) = parse_event(element).await? {
+            matches.push(game);
+        }
+    }
+
+    for game in matches {
+        println!("{:#?}", game);
+    }
+
+    // sleep(std::time::Duration::from_secs(10)).await;
+
+    driver.quit().await?;
+
+    Ok(())
+}
+
+#[derive(Debug)]
+struct MatchEvent {
+    home_team: String,
+    away_team: String,
+    time: DateTime<FixedOffset>,
+    location: String,
+}
+
+async fn parse_event(element: WebElement) -> anyhow::Result<Option<MatchEvent>> {
+    let teams = extract_teams(&element).await?;
+    if !teams.iter().any(|x| x == "BK Amager") {
+        return Ok(None);
+    }
+
+    let time = extract_time(&element).await?;
+    let location = extract_location(&element).await?;
+
+    let game = MatchEvent {
+        home_team: teams[0].to_owned(),
+        away_team: teams[1].to_owned(),
+        time,
+        location,
+    };
+
+    Ok(Some(game))
+}
+
+/// Extract the teams playing.
+async fn extract_teams(element: &WebElement) -> anyhow::Result<Vec<String>> {
+    let team_elements = element.query(By::ClassName("team-name-full")).all().await?;
+    let mut teams = Vec::new();
+    for team in team_elements {
+        teams.push(team.text().await.unwrap());
+    }
+
+    Ok(teams)
+}
+
+/// Extract the time of the match.
+/// This is always extracted in timezone +2.
+async fn extract_time(element: &WebElement) -> anyhow::Result<DateTime<FixedOffset>> {
+    let match_time_wrapper = element.query(By::ClassName("match-time")).first().await?;
+    let time_element = match_time_wrapper
+        .find(By::Tag("span"))
+        .await?
+        .text()
+        .await?;
+
+    parse_datetime(&time_element)
+}
+
+/// Extract the location for the match.
+async fn extract_location(element: &WebElement) -> anyhow::Result<String> {
+    let location_element = element.query(By::ClassName("venuename")).first().await?;
+    Ok(location_element.text().await?)
+}
+
+fn create_calendar() -> anyhow::Result<()> {
+    use chrono::{Duration, Utc};
     let event = Event::new()
         .summary("test event")
         .description("here I have something really important to do")
@@ -35,4 +135,37 @@ Hendrik"#,
     write!(output, "{}", calendar)?;
 
     Ok(())
+}
+
+fn parse_datetime(value: &str) -> anyhow::Result<DateTime<FixedOffset>> {
+    let pattern = "%b %d, %Y, %I:%M %p";
+
+    Ok(NaiveDateTime::parse_from_str(value, pattern)?
+        .and_local_timezone(FixedOffset::east_opt(2 * 3600).unwrap())
+        .single()
+        .unwrap())
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::{DateTime, FixedOffset, NaiveDate};
+
+    use crate::parse_datetime;
+
+    #[test]
+    fn parse_date() {
+        let s = "Sep 16, 2023, 2:30 PM";
+
+        let expected = NaiveDate::from_ymd_opt(2023, 9, 16)
+            .unwrap()
+            .and_hms_opt(12, 30, 0)
+            .unwrap();
+        assert_eq!(
+            parse_datetime(s).unwrap(),
+            DateTime::<FixedOffset>::from_naive_utc_and_offset(
+                expected,
+                FixedOffset::east_opt(2 * 3600).unwrap()
+            )
+        );
+    }
 }
